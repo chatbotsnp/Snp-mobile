8de4d7b0179c7fc8a6fd359b4e2e6be59d7dd7d6
chatbotsnp <trongbinhpro@gmail.com>
2025-08-29 16:49:46 +0700

    Create MainActivity.kt

---
+++ app_lib/android/app/android/app/src/main/kotlin/vn/snp/chatbot/MainActivity.kt
@@ -0,0 +1,6 @@
+package vn.snp.chatbot   // ⚠️ sửa cho đúng với package trong AndroidManifest + build.gradle
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
